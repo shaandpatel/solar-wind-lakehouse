5e4b0d81fc9a6bedefaa8bb12db5adc30df73dda
Shaan Patel <shaanpatel98@gmail.com>
2026-07-30 16:32:56 -0400

    change file path in config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 # Raw Data Source
-RAW_DATA_PATH = "/Volumes/workspace/default/raw_data/historicsw.csv"
+RAW_DATA_PATH = "/Workspace/Users/shaandpatel98@gmail.com/historicsw.csv"  # or file:/tmp/historicsw.csv
 
-# Delta Storage Paths
-BRONZE_PATH = "/tmp/swdb/bronze_sw"
-SILVER_PATH = "/tmp/swdb/silver_sw"
-QUARANTINE_PATH = "/tmp/swdb/quarantine_sw"
-GOLD_TRAINING_EVAL_PATH = "/tmp/swdb/gold_training_eval"
-GOLD_PATH = "/tmp/swdb/gold_anomalies"
+# Local Path-Based Delta Storage
+BRONZE_PATH = "file:/tmp/swdb/bronze_sw"
+SILVER_PATH = "file:/tmp/swdb/silver_sw"
+QUARANTINE_PATH = "file:/tmp/swdb/quarantine_sw"
+GOLD_TRAINING_EVAL_PATH = "file:/tmp/swdb/gold_training_eval"
+GOLD_PATH = "file:/tmp/swdb/gold_anomalies"
 
 # MLflow Experiment Configuration
 MLFLOW_EXPERIMENT_PATH = "/Users/shaandpatel98@gmail.com/solar-wind-lakehouse"
